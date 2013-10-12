8c75c7f4cb38347fea34b4f107e3217323d98ce3
Fernando Cortez <fccortez1@hotmail.com>
2013-10-12 12:01:05 -0500

    first C program

---
+++ matmul_B-replicated.c
@@ -0,0 +1,6 @@
+/*
+ * This MPI program multiplies two n by n matrices, A and B, with each
+ * processor used producing a row-band of matrix C. P0 will send row-bands
+ * of A and the entire matrix B to the slave processes.
+ */
+
