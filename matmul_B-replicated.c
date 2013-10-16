bfa0aa9c3b05cafbf22fa42a8ce916b65552c714
Fernando Cortez <fccortez1@hotmail.com>
2013-10-16 10:46:51 -0500

    Almost done. A few bugs to fix. Compiles and ansi compliant. Getting message APPLICATION TERMINATED WITH THE EXIT STRING: Hangup (signal 1)

--- matmul_B-replicated.c
+++ matmul_B-replicated.c
@@ -13,8 +13,11 @@
 #define MAXRAND 10000
 #define MINRAND 1
 
-int *calcBreakpoints(int n, int p);
-double *allocate_matrix(int n, int random);
+void matmul(double *A, double *B, double *C, int *share, int n, int id);
+void distribute_rows(double *matrix, int *share, int n, int p, int id);
+void collect_rows(double *matrix, int *share, int n, int p, int id);
+int *calc_breakpoints(int n, int p);
+double *allocate_matrix(int n, int random, char m);
 int *allocate_array(int n);
 double *free_matrix(double *v);
 int *free_array(int *v);
@@ -23,19 +26,15 @@ void print_array(int n, int *v);
 
 int main(int argc, char *argv[])
 {
-    int n;
-    int p;
-    int id;
-    int start;
-    int end;
-    int dest;
-    int source;
+    int n; /* dimensions of array */
+    int p; /* number of processors */
+    int id; /* rank of process */
+    int rows;
     double elapsed_time;
     double *matrixA;
     double *matrixB;
     double *matrixC;
     int *breakPoints;
-    MPI_Status status;
 
     switch( argc )
     {
@@ -53,27 +52,37 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
+    if(p < 2) {
+        printf("Need more processors!\n");
+        MPI_Finalize();
+        return 2; /* exit program with error */
+    }
+
     n = n>p ? n : p; /* ensures at least 1 row per processor */
-    breakPoints = calcBreakpoints(n, p);
+    breakPoints = calc_breakpoints(n, p);
 
     switch( id )
     {
         case 0:
-            matrixA = allocate_matrix(n*n, 1);
-            matrixB = allocate_matrix(n*n, 1);
-            matrixC = allocate_matrix(n*n, 0);
+            matrixA = allocate_matrix(n*n, 1, 'A');
+            matrixB = allocate_matrix(n*n, 1, 'B');
+            matrixC = allocate_matrix(n*n, 0, 'C');
+            elapsed_time = -MPI_Wtime();
             break;
         default:
-            matrixC = allocate_matrix(n*(breakPoints[id+1]-breakPoints[id]), 0);
+            rows = breakPoints[id+1] - breakPoints[id];
+            matrixC = allocate_matrix(n * rows, 0, 'c');
     }
 
-    elapsed_time = -MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
 
+    MPI_Bcast(matrixB, n*n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    distribute_rows(matrixA, breakPoints, n, p, id);
+    matmul(matrixA, matrixB, matrixC, breakPoints, n, id);
+    collect_rows(matrixC, breakPoints, n, p, id);
 
-
-    elapsed_time += MPI_Wtime();
-    
     if(!id) {
+        elapsed_time += MPI_Wtime();
         printf("Elapsed time: %6.10f\n", elapsed_time);
 
         if(n < 5) {
@@ -99,7 +108,73 @@ int main(int argc, char *argv[])
     return 0; /* exit program successfully */
 } /* end main */
 
-int *calcBreakpoints(int n, int p)
+void matmul(double *A, double *B, double *C, int *share, int n, int id)
+{
+    int i, j, k;
+    double temp;
+    int start = share[id];
+    int end = share[id+1];
+    int rows = end - start;
+    
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < n; j++) {
+            temp = A[i*n + j];
+            for(k = 0; k < n; k++) {
+                C[i*n + k] += temp * B[j*n + k];
+            }
+        }
+    }
+} /* end matrix multiplication */
+
+void distribute_rows(double *matrix, int *share, int n, int p, int id)
+{
+    int i, j, rows, start, end;
+    double *msg;
+    MPI_Status status;
+    switch( id )
+    {
+        case 0:
+            for(i = 1; i < p; i++) {
+                start = share[i];
+                end = share[i+1];
+                rows = end - start;
+                msg = allocate_matrix(n*rows, -1, 'm');
+                for(j = 0; j < n; j++)
+                    msg[j] = matrix[j + start * n];
+                MPI_Send(msg, n*rows, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
+                msg = free_matrix(msg);
+            }
+            break;
+        default:
+            rows = share[id+1] - share[id];
+            MPI_Recv(matrix, n*rows, MPI_DOUBLE, 0, id, MPI_COMM_WORLD, &status);
+    }
+} /* end row distribution */
+
+void collect_rows(double *matrix, int *share, int n, int p, int id)
+{
+    int i, j, rows, start, end;
+    double *msg;
+    MPI_Status status;
+    switch( id )
+    {
+        case 0:
+            for(i = 1; i < p; i++) {
+                start = share[i];
+                end = share[i+1];
+                rows = end - start;
+                MPI_Recv(msg, n*rows, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
+                for(j = 0; j < n; j++)
+                    matrix[j + start * n] = msg[j];
+                msg = free_matrix(msg);
+            }
+        default:
+            rows = share[id+1] - share[id];
+            MPI_Send(matrix, n*rows, MPI_DOUBLE, 0, id, MPI_COMM_WORLD);
+    }
+} /* end row collection */
+
+int *calc_breakpoints(int n, int p)
 {
     int i, *v; /* v is pointer to the vector */
     int remainder = n%p; /* stores excess rows not evenly distributed */
@@ -120,7 +195,7 @@ int *calcBreakpoints(int n, int p)
     return (v);
 } /* end calculate breakpoints */
 
-double *allocate_matrix(int n, int random)
+double *allocate_matrix(int n, int random, char m)
 {
     int i;
     double *v; /* v is pointer to the vector */
@@ -128,7 +203,7 @@ double *allocate_matrix(int n, int random)
     v = (double*) malloc(n * sizeof(double));
 
     if(v == NULL) {
-        printf("** Error in array allocation: insufficient memory **\n");
+        printf("** Error in matrix %c allocation: insufficient memory **\n", m);
         return (NULL);
     }
 
