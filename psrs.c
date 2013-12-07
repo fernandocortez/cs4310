276517a6254a00b1039919ec6fde255c788ba508
Fernando Cortez <fccortez1@hotmail.com>
2013-12-07 16:06:02 -0600

    Functions used to share the samples does not seem to work as intended

--- psrs.c
+++ psrs.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     if(argc != 2) {
         printf("**ERROR: Improper # of args\n");
         printf("Usage: mpirun -np [# procs] %s [size of array portion]\n", argv[0]);
-        exit(1);
+        exit(1); /* exit program with error */
     }
 
     MPI_Init(&argc, &argv);
@@ -42,8 +42,9 @@ int main(int argc, char *argv[])
 
     if(p < 2) {
         printf("Please input a larger number of processors\n");
+        printf("Usage: mpirun -np [# procs] %s [size of array portion]\n", argv[0]);
         MPI_Finalize();
-        exit(1);
+        exit(1); /* exit program with error */
     }
 
     m = (size_t) atoi(argv[1]);
@@ -60,10 +61,9 @@ int main(int argc, char *argv[])
 
     /* Initial sorting */
     qsort(local_array, m, sizeof(int), intcmp);
-    swap_samples(local_array, samples, (size_t) p, (size_t) id, m);
 
     /* Test printing */
-    if(m < 7)
+    if(m < 10)
         for(i = 0; i < p; i++) {
             MPI_Barrier(MPI_COMM_WORLD);
             if(id == i) {
@@ -74,18 +74,19 @@ int main(int argc, char *argv[])
             }
         }
 
-    psrs(local_array, samples, (size_t) p, (size_t) id, &m);
+    swap_samples(local_array, samples, (size_t) p, (size_t) id, m);
+    //psrs(local_array, samples, (size_t) p, (size_t) id, &m);
+    qsort(local_array, m, sizeof(int), intcmp);
 
     /* Test printing */
-    if(m < 7)
-        for(i = 0; i < p; i++) {
-            MPI_Barrier(MPI_COMM_WORLD);
-            if(id == i) {
-                printf("This is process %d local array\n", id);
-                print_array(local_array, m, (char) id);
-                printf("\n");
-            }
+    for(i = 0; i < p; i++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(id == i && m < 10) {
+            printf("This is process %d local array\n", id);
+            print_array(local_array, m, (char) id);
+            printf("\n");
         }
+    }
 
     /* Deallocate memory */
     local_array = free_array(local_array);
@@ -97,19 +98,27 @@ int main(int argc, char *argv[])
 
 void psrs(int *v, int *s, size_t p, size_t id, size_t *m)
 {
-    size_t i;
+    size_t i, n = *m * p;
     size_t sample_size = p-1;
     size_t ptr_count = p+1;
-    int **sample_ptr = (int**) malloc(ptr_count * sizeof(int*));
+    int *sample_ptr[ptr_count];
+    int inc_msg[sample_size][n];
+    int out_msg[sample_size][n];
+    size_t inc_msg_size[sample_size];
+    size_t out_msg_size[sample_size];
     int temp;
+    int source;
+    int dest;
+    MPI_Status status;
 
-    for(i = 0; i < ptr_count; i++)
+    for(i = 0; i < p; i++)
         sample_ptr[i] = v; /* all pointers point to first element of array */
-    sample_ptr[p] = &v[m-1]; /* last pointer at last element of array */
-    for(i = 1; i < sample_size; i++) {
+    sample_ptr[p] = &v[*m-1]; /* last pointer at last element of array */
+    for(i = 1; i < p; i++) {
         temp = s[i];
         while(*sample_ptr[i]++ < temp);
     }
+
     *m = sample_ptr[id+1] - sample_ptr[id];
     for(i = 0; i < p; i++) {
         if(id == i) {
@@ -117,51 +126,36 @@ void psrs(int *v, int *s, size_t p, size_t id, size_t *m)
         }
     }
 
-    /* Deallocate memory */
     for(i = 0; i < ptr_count; i++)
         sample_ptr[i] = NULL;
-    free(sample_ptr);
-    sample_ptr = NULL;
 } /* end parallel sorting by sampling */
 
 void swap_samples(int *v, int *s, size_t p, size_t id, size_t m)
 {
-    size_t i, step = m/p, temp;
+    size_t i, step = m/p;
+    size_t sample_size = p-1;
     int *msg;
-    MPI_Status status;
 
-    if(!id)
-        for(i = 0; i < p-1; i++)
-            s[i] = v[i * step];
+    for(i = 1; i < p; i++)
+        s[i-1] = v[i * step];
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    /* all processors send samples to processor 0 */
+    MPI_Gather(s, sample_size, MPI_INT, s, sample_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     if(!id) {
-        temp = p * (p-1);
-        /* p0 receives samples from other processors */
-        for(; i < temp; i++)
-            MPI_Recv(&s[i], 1, MPI_INT, i, i, MPI_COMM_WORLD, &status);
-
-        qsort(s, temp, sizeof(int), intcmp);
-        
+        qsort(s, p * sample_size, sizeof(int), intcmp);
         /* isolates samples all processors will use */
-        msg = allocate_array(p-1, 0, 'm');
-        step = p-1;
-        temp = step;
+        msg = allocate_array(sample_size, 0, 'm');
         for(i = 1; i < p; i++)
-            msg[i] = s[i * step];
-        for(i = 1; i < p; i++)
-            MPI_Send(msg, temp, MPI_INT, i, i, MPI_COMM_WORLD);
+            msg[i-1] = s[i * sample_size];
         s = free_array(s);
         s = msg;
         msg = free_array(msg);
-    } else {
-        /* send samples to p0 */
-        for(i = 1; i < p; i++)
-            MPI_Send(&v[i*step], 1, MPI_INT, 0, id, MPI_COMM_WORLD);
-        temp = p-1;
-        MPI_Recv(s, temp, MPI_INT, 0, id, MPI_COMM_WORLD, &status);
     }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    /* broadcast samples to all processors */
+    MPI_Bcast(s, sample_size, MPI_INT, 0, MPI_COMM_WORLD);
 } /* end swap samples */
 
 int intcmp(const void *p1, const void *p2)
